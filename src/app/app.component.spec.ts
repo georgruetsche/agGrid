e69853cf709cb9f81834821e53ef9124641970ad
Rütsche Georg (rutc) <rutc@zhaw.ch>
2020-12-10 09:19:37 +0100

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        AgGridModule.withComponents([])
+    ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+ 
   });
 
   it('should create the app', () => {
@@ -28,4 +35,17 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('app-project-ag-grid app is running!');
   });
+
+  it('grid API is not available until  `detectChanges`', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    expect(component.agGrid.api).not.toBeTruthy();
+});
+
+it('grid API is available after `detectChanges`', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.agGrid.api).toBeTruthy();
+});
 });
