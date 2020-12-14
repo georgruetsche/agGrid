67d5b5557c34077a0302be34dcce570991f9ff1a
ZHAW\rutc <rutc@zhaw.ch>
2020-12-14 16:58:25 +0100

    add layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 import 'ag-grid-enterprise';
 
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -14,21 +15,76 @@ export class AppComponent implements OnInit {
     title = 'AppProjectAgGrid';
 
     columnDefs = [
-        { field: 'make', sortable: true, checkboxSelection: true, filter: 'agTextColumnFilter', floatingFilter: true},
-        { field: 'model', sortable: true, filter: 'agTextColumnFilter', floatingFilter: true },
-        { field: 'price', sortable: true, filter: 'agTextColumnFilter', floatingFilter: true },
-        { field: 'available', sortable: true, filter: 'agTextColumnFilter', floatingFilter: true },
+        { headerName: 'OE FS II', showRowGroup: 'OE FS II', cellRenderer: 'agGroupCellRenderer', minWidth: 200},
+        { field: 'OE FS II', rowGroup: true, hide: true, suppressColumnsToolPanel: true},
+        { headerName: 'OE FS III', showRowGroup: 'OE FS III', cellRenderer: 'agGroupCellRenderer', minWidth: 200},
+        { field: 'OE FS III', rowGroup: true, hide: true, suppressColumnsToolPanel: true},
+        { field: 'Mitarbeitername', rowGroup: false, hide: false, sortable: true, suppressColumnsToolPanel: false, resizable: true, suppressSizeToFit: true},
+        {headerName: 'DL',
+        children: [
+        { field: 'ILV', rowGroup: false, hide: false, sortable: true, resizable: true},
+        { field: 'Zeus', rowGroup: false, hide: false, sortable: true, resizable: true},
+        { field: 'Delta', rowGroup: false, hide: false, sortable: true, resizable: true},
+        { field: 'Delta in %', rowGroup: false, hide: false, sortable: true, resizable: true,}
+        ]
+    },
+        {headerName: 'F&E',
+            children: [
+            { field: 'ILV', rowGroup: false, hide: false, sortable: true, resizable: true},
+            { field: 'Zeus', rowGroup: false, hide: false, sortable: true, resizable: true },
+            { field: 'Delta', rowGroup: false, hide: false, sortable: true, resizable: true },
+            { field: 'Delta in %', rowGroup: false, hide: false, sortable: true, resizable: true  }
+            ]
+        },
+        {headerName: 'Interne Projekte',
+        children: [
+        { field: 'ILV', rowGroup: false, hide: false, sortable: true, resizable: true},
+        { field: 'Zeus', rowGroup: false, hide: false, sortable: true, resizable: true },
+        { field: 'Delta', rowGroup: false, hide: false, sortable: true, resizable: true },
+        { field: 'Delta in %', rowGroup: false, hide: false, sortable: true, resizable: true  }
+        ]
+    },
+        {headerName: 'WB',
+        children: [
+        { field: 'ILV', rowGroup: false, hide: false, sortable: true, resizable: true },
+        { field: 'Zeus', rowGroup: false, hide: false, sortable: true, resizable: true },
+        { field: 'Delta', rowGroup: false, hide: false, sortable: true, resizable: true },
+        { field: 'Delta in %', rowGroup: false, hide: false, sortable: true, resizable: true }
+        ]
+}
+        
+
     ];
 
+
     rowData: any;
+    
+    groupMultiAutoColumn: true;
+
 
     constructor(private http: HttpClient) {
 
     }
 
+    
+
     ngOnInit() {
-        this.rowData = this.http.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json');
+        this.rowData = [
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.', 'Mitarbeitername': 'Braun Markus'  },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.', 'Mitarbeitername': 'Kessler Esther'  },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.', 'Mitarbeitername': 'Sun Wei'  },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W FS Corporate Finance & Private Equity' },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Lehre Banking, Finance, Insurance' },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Zentrum für Accounting&Controlling' },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Zentrum für Banking & Finance' },
+            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Zentrum für Risk & Insurance' },
+            
+        ];
     }
+
+    onGridReady() {
+        this.agGrid.gridOptions.api.sizeColumnsToFit();
+      }
 
     getSelectedRows() {
         const selectedNodes = this.agGrid.api.getSelectedNodes();
