5433105cc2b4336469a030b609f69d0ea0f9881f
ZHAW\rutc <rutc@zhaw.ch>
2020-12-17 14:43:42 +0100

    change structur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 import 'ag-grid-enterprise';
-import { threadId } from 'worker_threads';
+
 
 
 @Component({
@@ -24,46 +24,46 @@ export class AppComponent implements OnInit {
         { field: 'OE FS III', rowGroup: true, hide: true, suppressColumnsToolPanel: true},
         { field: 'Mitarbeitername', rowGroup: false, hide: false, sortable: true, suppressColumnsToolPanel: false, resizable: true, suppressSizeToFit: true, tooltipField: 'Mitarbeitername'},
         ]},
+
         {headerName: 'DL',
         children: [
         { headerName: 'ILV', field: 'DL.ILV', hide: false, sortable: true, resizable: true, tooltipField: 'ILV', aggFunc: 'sum', enableValue: true },
         { headerName: 'Zeus', field: 'DL.Zeus', hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
         { headerName: 'Δ', field: 'DL.Δ', hide: false, sortable: true, resizable: true, valueGetter: this.deltaValueGetter, enableValue: true},
         { headerName: 'Δ%', field: 'DL.Δ%', hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
-        ]
-    },
+        ]},
+
         {headerName: 'FE',
             children: [
             { headerName: 'ILV', field: 'FE.ILV', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
             { headerName: 'Zeus', field: 'FE.Zeus',  rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
             { headerName: 'Δ', field: 'FE.Δ', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.deltaValueGetter, enableValue: true},
             { headerName: 'Δ%', field: 'FE.Δ%',rowGroup: false, hide: false, sortable: true, aggFunc: 'sum', resizable: true, enableValue: true}
-            ]
-        },
+            ]},
+
         {headerName: 'Interne_Projekte',
         children: [
         { headerName: 'ILV', field: 'Interne_Projekte.ILV', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
         { headerName: 'Zeus', field: 'Interne_Projekte.Zeus', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
         { headerName: 'Δ', field: 'Interne_Projekte.Δ', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.deltaValueGetter, enableValue: true},
         { headerName: 'Δ%', field: 'Interne_Projekte.Δ%', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true}
-        ]
-    },
+        ]},
+
         {headerName: 'WB',
         children: [
         { headerName: 'ILV', field: 'WB.ILV', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
         { headerName: 'Zeus', field: 'WB.Zeus', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true},
         { headerName: 'Δ', field: 'WB.Δ', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.deltaValueGetter, enableValue: true},
         { headerName: 'Δ%', field: 'WB.Δ%', rowGroup: false, hide: false, sortable: true, resizable: true, aggFunc: 'sum', enableValue: true}
-        ]
-},
+        ]},
+
         {headerName: 'Gesamtergebnis', 
         children: [
         { headerName: 'ILV', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.totalValueGetter, enableValue: true},
         { headerName: 'Zeus', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.totalValueGetter, enableValue: true},
         { headerName: 'Δ', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.totalValueGetter, enableValue: true},
         { headerName: 'Δ%', rowGroup: false, hide: false, sortable: true, resizable: true, valueGetter: this.totalValueGetter, enableValue: true}
-        ]
-},
+        ]},
     ];
 
 
@@ -82,7 +82,7 @@ export class AppComponent implements OnInit {
             values = ['FE.ILV', 'FE.Zeus'];
         }
         if (params.colDef.field === "Interne_Projekte.Δ") {
-            values = ['Interne_Projekt.ILV', 'Interne_Projekte.Zeus'];
+            values = ['Interne_Projekte.ILV', 'Interne_Projekte.Zeus'];
         }
         if (params.colDef.field === "WB.Δ") {
             values = ['WB.ILV', 'WB.Zeus'];
@@ -103,15 +103,17 @@ export class AppComponent implements OnInit {
             return params.getValue(values[0]) + params.getValue(values[1]) + params.getValue(values[2]) + params.getValue(values[3]); 
         }
         if (params.colDef.headerName === "Δ") {
-            const valuesILV = ['DL.ILV', 'FE.ILV', 'Interne_Projekte.IL', 'WB.ILV'];
+            const valuesILV = ['DL.ILV', 'FE.ILV', 'Interne_Projekte.ILV', 'WB.ILV'];
             const valuesZeus = ['DL.Zeus', 'FE.Zeus', 'Interne_Projekte.Zeus', 'WB.Zeus'];
             return (params.getValue(valuesILV[0]) + params.getValue(valuesILV[1]) + params.getValue(valuesILV[2]) + params.getValue(valuesILV[3])) -  (params.getValue(valuesZeus[0]) + params.getValue(valuesZeus[1]) + params.getValue(valuesZeus[2]) + params.getValue(valuesZeus[3])); 
         }
         if (params.colDef.headerName === "Δ%") {
             const valuesDelta = ['DL.Δ', 'FE.Δ', 'Interne_Projekte.Δ', 'WB.Δ'];
             const valuesILV = ['DL.ILV', 'FE.ILV', 'Interne_Projekte.ILV', 'WB.ILV'];
-            const dif = ((params.getValue(valuesDelta[0]) + params.getValue(valuesDelta[1]) + params.getValue(valuesDelta[2]) + params.getValue(valuesDelta[3])) / (params.getValue(valuesILV[0]) + params.getValue(valuesILV[1]) + params.getValue(valuesILV[2]) + params.getValue(valuesILV[3]))) * 100; 
-            return dif;
+            const totalDelta = params.getValue(valuesDelta[0]) + params.getValue(valuesDelta[1]) + params.getValue(valuesDelta[2]) + params.getValue(valuesDelta[3]);
+            const totalILV =  params.getValue(valuesILV[0]) + params.getValue(valuesILV[1]) + params.getValue(valuesILV[2]) + params.getValue(valuesILV[3]);
+            const totalDif = (totalDelta / totalILV) * 100;
+            return Math.round(totalDif);
         }
     }
 
