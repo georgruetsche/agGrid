aa1321d557de2e9e7260ba91ba348e0f6c58a824
ZHAW\rutc <georgruetsche@gmail.com>
2021-01-07 15:18:20 +0100

    outsource row-data-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 import 'ag-grid-enterprise';
 import { FormControl, FormGroup } from '@angular/forms';
+import { RowDataService } from './shared/row-data-service';
 
 
 interface ExcelExportParams {
@@ -155,22 +156,11 @@ export class AppComponent implements OnInit {
         }
     }
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private rowDataService: RowDataService) {
     }
 
     ngOnInit(): void {
-        this.rowData = [
-            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.',
-            Mitarbeitername: 'Hans Muster', DL: {ILV: 0, Zeus: -40, 'Δ%': 0 }, FE: {ILV: -192, Zeus: -60, 'Δ%': 69 },
-            Interne_Projekte: {ILV: -843, Zeus: -600, 'Δ%': 29 }, WB: {ILV: -24, Zeus: -8, 'Δ%': 69 }},
-            { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.',
-            Mitarbeitername: 'Peter Muster', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: -17, Zeus: 0, 'Δ%': 100 },
-            Interne_Projekte: {ILV: -42, Zeus: -70, 'Δ%': -66 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
-            { 'OE FS II': 'W Abteilung Business Law Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt', Mitarbeitername: 'Petra Müller', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 }, Interne_Projekte: {ILV: -55, Zeus: -75, 'Δ%': -37 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
-            { 'OE FS II': 'W Abteilung Business Law Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt', Mitarbeitername: 'Ramon Müller', DL: {ILV: -63, Zeus: 0, 'Δ%': 100}, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 }, Interne_Projekte: {ILV: -278, Zeus: -618, 'Δ%': -123 }, WB: {ILV: -300, Zeus: 0, 'Δ%': 100 }},
-            { 'OE FS II': 'W Abteilung Business Test Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt', Mitarbeitername: 'Petra Müller', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 }, Interne_Projekte: {ILV: -55, Zeus: -75, 'Δ%': -37 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
-            { 'OE FS II': 'W Abteilung Business Test Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt', Mitarbeitername: 'Ramon Müller', DL: {ILV: -63, Zeus: 0, 'Δ%': 100}, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 }, Interne_Projekte: {ILV: -278, Zeus: -618, 'Δ%': -123 }, WB: {ILV: -300, Zeus: 0, 'Δ%': 100 }},
-          ];
+        this.rowData = this.rowDataService.getAllData();
     }
 
     onGridReady(): void {
---
+++ src/app/shared/row-data-service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RowDataService {
+  rowData: any[];
+
+  constructor() {
+    this.rowData = [
+      { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.',
+      Mitarbeitername: 'Hans Muster', DL: {ILV: 0, Zeus: -40, 'Δ%': 0 }, FE: {ILV: -192, Zeus: -60, 'Δ%': 69 },
+      Interne_Projekte: {ILV: -843, Zeus: -600, 'Δ%': 29 }, WB: {ILV: -24, Zeus: -8, 'Δ%': 69 }},
+      { 'OE FS II': 'W Abteilung Banking, Finance, Insurance Gesamt', 'OE FS III': 'W Abt. Banking, Finance, Insurance Ltg.',
+      Mitarbeitername: 'Peter Muster', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: -17, Zeus: 0, 'Δ%': 100 },
+      Interne_Projekte: {ILV: -42, Zeus: -70, 'Δ%': -66 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
+      { 'OE FS II': 'W Abteilung Business Law Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt',
+      Mitarbeitername: 'Petra Müller', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 },
+      Interne_Projekte: {ILV: -55, Zeus: -75, 'Δ%': -37 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
+      { 'OE FS II': 'W Abteilung Business Law Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt',
+      Mitarbeitername: 'Ramon Müller', DL: {ILV: -63, Zeus: 0, 'Δ%': 100}, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 },
+      Interne_Projekte: {ILV: -278, Zeus: -618, 'Δ%': -123 }, WB: {ILV: -300, Zeus: 0, 'Δ%': 100 }},
+      { 'OE FS II': 'W Abteilung Business Test Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt',
+      Mitarbeitername: 'Petra Müller', DL: {ILV: 0, Zeus: 0, 'Δ%': 0 }, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 },
+      Interne_Projekte: {ILV: -55, Zeus: -75, 'Δ%': -37 }, WB: {ILV: 0, Zeus: 0, 'Δ%': 0 }},
+      { 'OE FS II': 'W Abteilung Business Test Gesamt', 'OE FS III': 'W Abteilung Business Law Ltg. Gesamt',
+      Mitarbeitername: 'Ramon Müller', DL: {ILV: -63, Zeus: 0, 'Δ%': 100}, FE: {ILV: 0, Zeus: 0, 'Δ%': 0 },
+      Interne_Projekte: {ILV: -278, Zeus: -618, 'Δ%': -123 }, WB: {ILV: -300, Zeus: 0, 'Δ%': 100 }},
+    ];
+   }
+   // tslint:disable-next-line: typedef
+   getAllData() {
+     return this.rowData;
+   }
+}
